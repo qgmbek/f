d80f66f55677cd13fc0a373b574222218f41acd6
Qogambek <100270972+qgmbek@users.noreply.github.com>
2024-12-10 21:38:32 +0700

    Add files via upload

---
+++ r1/r1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace r1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9')) return; 
+
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ','; 
+            }
+
+            if (e.KeyChar == ',')
+            {
+                if (textBox1.Text.IndexOf(',') != -1) e.Handled = true; 
+                return;
+            }
+
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter) button1.Focus(); 
+                return;
+            }
+
+            e.Handled = true; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double mile;
+            double km;
+
+            try
+            {
+                mile = Convert.ToDouble(textBox1.Text);
+                km = mile * 1.609344;
+                label2.Text = km.ToString("n") + " км.";
+            }
+            catch
+            {
+                textBox1.Focus(); 
+            }
+        }
+    }
+}
---
+++ r2/r2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace r2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+                return;
+
+            if (e.KeyChar == '.')
+                e.KeyChar = ',';
+
+            if (e.KeyChar == ',')
+            {
+                if ((textBox1.Text.IndexOf(',') != -1) || (textBox1.Text.Length == 0))
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                    button1.Focus();
+                return;
+            }
+
+            e.Handled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            button1.Enabled = textBox1.Text.Length > 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double funt;
+            double kg;
+
+            if (double.TryParse(textBox1.Text, out funt))
+            {
+                kg = funt * 0.4095;
+                label2.Text = funt.ToString("N") + " ф. = " + kg.ToString("N") + " кг.";
+            }
+            else
+            {
+                MessageBox.Show("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+}
