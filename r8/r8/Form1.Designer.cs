504555a70c87797f37b66fafdb5b456d8aae2380
Qogambek <100270972+qgmbek@users.noreply.github.com>
2024-12-10 21:43:22 +0700

    Add files via upload

---
+++ r8/r8/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace r8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            pictureBox1 = new PictureBox();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            label1 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(118, 112);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(235, 144);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(460, 105);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(263, 167);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // folderBrowserDialog1
+            // 
+            folderBrowserDialog1.SelectedPath = "C:\\Users\\hogam\\source\\repos\\images";
+            folderBrowserDialog1.HelpRequest += folderBrowserDialog1_HelpRequest;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(111, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Folder Path";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(275, 308);
+            button1.Name = "button1";
+            button1.Size = new Size(162, 29);
+            button1.TabIndex = 3;
+            button1.Text = "Select a folder";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private PictureBox pictureBox1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Label label1;
+        private Button button1;
+    }
+}
